166cedff836147372aa9377ac0890c5e4da108b0
rosemary chinasa <okorochinasa20@gmail.com>
2018-07-05 12:11:40 +0100

    result checker portal

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 
 ?>
 
+<script src="jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
 <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="js/bootstrap.min.js"></script>
 <script src="jquery/jquery-2.2.4.min.js"></script>
@@ -31,38 +34,79 @@
 		<link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
 
-		<title>LSMJC-RESULT PORTAL</title>
+		<title>LSMSC-RESULT PORTAL</title>
         
         <style>
          a.red{
-            color:#669d89 !important;
-            font-size: 18px;
+            color:black!important;
+            font-family: sans-serif;
+            font-style: normal;
+            font-size: 16px;
+    
          }
+         
          
          a:hover{
-            color: #669d89;
-            background-color: ;
-         }
-         
-         li>a.active{
-            color: white !important;
-         }
-         a.active:hover{
-            background-color:#f27c7e !important;
+            color:green !important;
          }
          button.color{background-color: green !important;
          }
+         /* Dropdown Content (Hidden by Default) */
+.dropdown-menu {
+    display: none;
+    position: absolute;
+    background-color:#f4df90;
+    max-width: 250px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+    
+    
+}
+
+/* Links inside the dropdown */
+.dropdown-menu a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-menu:hover {background-color:#f4df90;}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-menu{
+    display: block;
+}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.dropdown:hover .dropdown-toggle.fall {
+    background-color:red;}
+    
+    .nav>li>a:focus, .nav>li>a:hover {
+	text-decoration: none;
+    border-top: 1px solid #c1a021;
+}
+
+    .fall:hover{
+color: #000 !important;
+background-color:green !important;
+
+    }
+    
+    
          
         </style>
 	</head>
 	<body style="background-image: url(images/back.png); overflow-x: hidden;">
     
     <div class="header3" style="width:100%;">
-    <a href="http://lsmjcmeiran.org/"><img  src="images/lagos.png" width="100%"/> </a> 
+    <a href="http://lsmscmeiran.org/"><img  src="images/senior.PNG" width="100%"/> </a> 
     
     <div class="nav">
   
    <nav class="navbar navbar-inverse main-menu"  style="background-color:#f4df90; border: none;">
+   <hr  style="width:90%;" />
   <div class="container-fluid text-center">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle color" data-toggle="collapse" data-target="#myNavbar">
@@ -73,24 +117,44 @@
       
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav" style="padding-left: 90px;">
+      <ul class="nav navbar-nav navbar-left">
         
-            <li > <a href="http://lsmjcmeiran.org/" class="red">Home</a></li> 
-            <li> <a href="http://lsmjcmeiran.org/index.php/about/" class="red"> About  </a> </li> 
-            <li> <a href="http://lsmjcmeiran.org/index.php/academics/" class="red">Academics</a></li> 
-            <li>  <a href="http://lsmjcmeiran.org/index.php/gallery/" class="red">Gallery</a></li> 
-            <li> <a href="http://lsmjcmeiran.org/index.php/parents-forum/" class="red">Parents&apos; Forum</a></li>
-            <li> <a href="http://portal.lsmjcmeiran.org/" class="active red">Result Portal </a></li>
-            <li> <a href="http://lsmjcmeiran.org/index.php/news/" class="red">News</a> </li>
-            <li><a href="http://lsmjcmeiran.org/index.php/contact/" class="red">Contact</a></li>
+            <li > <a href="http://lsmscmeiran.org/" class="red">Home</a></li> 
+            <li class="dropdown"> <a href="http://lsmscmeiran.org/index.php/about-us/" class="dropdown-toggle red" data-toggle="dropdown"> About Us
+            <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+            <li style="text-align: left;"> <a href="http://lsmscmeiran.org/about-us/" class="fall">Our History</a></li>
+            <li style="text-align: left;"><a href="http://lsmscmeiran.org/about-us/management-and-staff/" class="fall">Management and Staff</a> </li>
+            </ul>
+            </li> 
+            <li  class="dropdown"> <a href="http://lsmscmeiran.org/single-class-alt/" class="dropdown-toggle red" data-toggle="dropdown">Academics
+            <span class="caret"></span></a>
+            
+            <ul class="dropdown-menu">
+            <li style="text-align: left;"> <a href="http://lsmscmeiran.org/single-class-alt/" class="fall">Academic Brief</a> </li> 
+            <li style="text-align: left;"> <a href="http://lsmscmeiran.org/single-class-alt/" class="fall">Curriculum Development </a> </li>
+            <li style="text-align: left;"> <a href="http://lsmscmeiran.org/single-class-alt/" class="fall"> Practical Work </a></li>
+            <li style="text-align: left;"> <a href="http://lsmscmeiran.org/single-class-alt/" class="fall">Extral Curricular </a></li>
+            </ul></li>
+            <li>  <a href="http://lsmscmeiran.org/news/" class="red">News </a></li>
+            
+            <li> <a href="http://portal.lsmscmeiran.org/" class=" red">Result Portal </a></li>
+            <li><a href="http://lsmscmeiran.org/contact/" class="red">Contact</a></li>
         </ul>
-      
+        
+        <a href="http://lsmscmeiran.org/#"><i class="fa fa-twitter" style="background-color: #55acee; color: white; border-radius: 50%; font-size: 20px; padding: 15px;"> </i> </a> 
+        <a href="http://lsmscmeiran.org/#"><i class="	fa fa-facebook-f" style="background-color:#5472d2; color: #fff; border-radius: 50%; font-size: 20px; padding: 15px;"> </i> </a>
+           <a href="http://lsmscmeiran.org/#"><i class="fa fa-youtube" style="background-color:#e52d27; color: #fff; border-radius: 50%; font-size: 20px; padding: 15px;"> </i></a>
+          <a href="http://lsmscmeiran.org/#"> <i class="fa fa-google-plus" style="background-color:#dd4b39; color: #fff; border-radius: 50%; font-size: 20px; padding: 15px;"> </i> </a>
+            
     </div>
   </div>
 </nav>
+    <hr  style="color:red; width:100%; border: 3px solid green; margin-top: -20px;"/>
+    </div>
+     
+    </div>
     
-    </div>
-    </div>
     
     
      
@@ -157,9 +221,10 @@
 									<span class="input-group-addon"><i class="fa fa-graduation-cap fa-lg" aria-hidden="true"></i></span>
 								<select name="classes" class="form-control" id="password"  required="required">
                               <option value="0">Select your class</option>
-                              <option value="JSS1">JSS 1 </option>
-                              <option value="JSS2">JSS 2 </option>
-                              <option value="JSS3">JSS 3 </option>
+                              <option value="SSS1">SSS 1 </option>
+                              <option value="SSS2">SSS 2 </option>
+                              
+                              
                              </select>
 								</div>
 							</div>
